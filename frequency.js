6361f051bd074163627e7c47bef0c30a84878e8c
Your Name <you@example.com>
2020-03-16 08:30:51 -0500

    edit

--- frequency.js
+++ frequency.js
@@ -6,7 +6,7 @@ document.getElementById("countButton").onclick = function () {
     typedText = typedText.replace(/[^a-z'\s]+/g, "");
     word = typedText.split(/\s/);
 
-    for (let i = 0; i < word.length; i++) {
+    for (let i = 0; i < typedText.length; i++) {
         currentLetter = typedText[i];
         if (letterCounts[currentLetter] === undefined) {
             letterCounts[currentLetter] = 1;
